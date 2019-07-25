e52b7ca4e8b3a0e8558ba9056bff56abf921b400
zhyjor <zhyjor@163.com>
2019-07-25 13:54:33 +0800

    add:program one

---
+++ daily/2019-07-25/1_tow_sum/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let x = 0; x < nums.length; x++) {
+    for (let y = x + 1; y < nums.length; y++) {
+      if (nums[x] + nums[y] === target) {
+        return [x, y];
+      }
+    }
+  }
+  return null;
+};
+
+const nums = [2, 7, 11, 15], target = 13;
+console.log(twoSum(nums, target));
