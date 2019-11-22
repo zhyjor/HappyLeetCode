fd89c62c9de4f9d4a8709462518d2f802fa4aee0
zhyjor <zhyjor@163.com>
2019-11-22 19:41:18 +0800

    fix

--- daily/2019-11-15/10-regular-expression-matching/index.js
+++ daily/2019-11-15/10-regular-expression-matching/index.js
@@ -4,10 +4,11 @@
  * @return {boolean}
  */
 var isMatch = function(s, p) {
-  var lens =s.length;
-  var lenP = p.length;
-  var map = {};
-
-
-
+  if(p.length === 0) { return s.length === 0 }
+    let first_match = (s.length !==0 && (s[0] === p[0] || p[0] === '.'))
+    if(p.length >= 2 && p[1] === '*') {
+        return isMatch(s, p.substring(2)) || (first_match && isMatch(s.substring(1), p))
+    } else {
+        return first_match && isMatch(s.substring(1), p.substring(1))
+    }
 };
