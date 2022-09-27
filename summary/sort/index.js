43be3492846c5c3056af32de2935b8b98a79567d
zhyj <zhyj@zhyjs-MacBook-Pro.local>
2022-09-27 18:58:59 +0800

    add: presign oss

---
+++ summary/sort/index.js
@@ -0,0 +1,16 @@
+function bubbleSort(numbers) {
+  const len = numbers.length;
+  // 每次找到第一个最大的
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len - i - 1; j++) {
+      if (numbers[j] > numbers[j + 1]) {
+        [numbers[j], numbers[j + 1]] = [numbers[j + 1], numbers[j]]
+      }
+    }
+  }
+  return numbers;
+}
+
+const array = [2, 3, 4, 1, 2, 5];
+
+console.log('bubbleSort:', bubbleSort(array));
