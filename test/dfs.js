13c76f6ada705d27c41d612c326da2c1302d4336
zhyj <zhyj@zhyjs-MacBook-Pro.local>
2022-12-23 17:03:23 +0800

    feat: dfs

--- test/dfs.js
+++ test/dfs.js
@@ -10,6 +10,8 @@ function permute(numbers) {
       res.push(curr.slice());
       return;
     }
+
+    // 有剩余的数字的时候才会往数组内丢
     for (let i = 0; i < len; i++) {
       // 先判断当前数字是否被用过
       if (!visited[numbers[i]]) {
@@ -30,4 +32,43 @@ function permute(numbers) {
   return res;
 }
 
-console.log(permute([1, 2, 3]));
+function subsets(numbers) {
+  const res = [];
+  const len = numbers.length;
+  const subset = [];
+  dfs(0);
+  function dfs(index) {
+    // 所有的记录入口
+    res.push(subset.slice());
+    for (let i = index; i < len; i++) {
+      subset.push(numbers[i]);
+      dfs(i + 1);
+      // 这是当前数字不存在与组合中的情况
+      subset.pop();
+    }
+    console.log(index, subset);
+  }
+  return res;
+}
+
+function combine(n, k) {
+  const res = [];
+  const subset = [];
+  dfs(1);
+  function dfs(index) {
+    // 所有的记录入口
+    if (subset.length === k) {
+      res.push(subset.slice());
+      return;
+    }
+    for (let i = index; i <= n; i++) {
+      subset.push(i);
+      dfs(i + 1);
+      // 这是当前数字不存在与组合中的情况
+      subset.pop();
+    }
+  }
+  return res;
+}
+
+console.log(combine(4, 2));
